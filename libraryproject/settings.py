77850a1e9282ebaf6220b180354cc91269037f97
Shada <shada@Shadas-MacBook.local>
2026-03-30 20:34:04 +0300

    Update Lab 5

--- libraryproject/settings.py
+++ libraryproject/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'libraryproject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'], # أضيفي هذا السطر ليقرأ المجلد الرئيسي للقوالب
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,3 +118,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / "static"]
